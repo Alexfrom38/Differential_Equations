fb93d95e166fbc99643fa1eefc8c9da6d16ae43e
Alexander Martynov <56801896+Alexfrom38@users.noreply.github.com>
2023-04-12 18:48:14 +0300

    Update Main.cpp

--- Differential_Equations/Main.cpp
+++ Differential_Equations/Main.cpp
@@ -7,21 +7,11 @@ void Insert_In_File(double* new_array, size_t count, std::fstream& stream)
 	{
 		for (size_t i = 0; i < count; i++)    
 			stream << new_array[i] << " ";
-		stream << std::endl;
-		stream << std::endl;
+		stream << "\n";
+		//stream << "\n";
 	}
 	else
 		throw "The file isn't exist";
-}
-
-bool IsGoodTime(int i)
-{
-	if (i / 100000 == 0 && i % 10 == 0 && (i / 10) % 10 == 0 && (i / 100) % 10 == 0 && (i / 1000) % 10 == 0 &&
-		((i / 10000) % 10 == 1 || (i / 10000) % 10 == 2 || (i / 10000) % 10 == 3 || (i / 10000) % 10 == 4 || (i / 10000) % 10 == 5 || (i / 10000) % 10 == 6 ||
-			(i / 10000) % 10 == 7 || (i / 10000) % 10 == 8 || (i / 10000) % 10 == 9 || (i / 10000) % 10 == 0))
-		return true;
-	else
-		return false;
 }
 
 
@@ -73,12 +63,13 @@ int main()
 	
 	for (size_t q = 0; q < count; q++)
 		initial[q] = sin(PI*deltaX*q);
-
+	
 	initial[count - 1] = 0.0;
+	
 
 	Insert_In_File(initial, count,f);
 
-	while (time < 1.0)
+	while (time < T_max)
 	{
 		time = deltaT * i;
 		for (size_t q = 1; q < count; q++)
@@ -95,12 +86,10 @@ int main()
 		for (size_t q = 0; q < count; q++)
 			initial[q] = main_array[q];
 	
-		if (IsGoodTime(i))
-		{
+		
+		if (i % 10000 == 0)
 			Insert_In_File(main_array, count, f);
-		}
-
-			i++;
+		i++;
 	}
 
 	Insert_In_File(main_array, count, f);
